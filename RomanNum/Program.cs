cbbd5c5a627f21f1c3b8b441733155c8b95f1344
MveloSK <mvelokhumalo55@gmail.com>
2025-10-27 00:02:56 +0200

    First test commit failed

---
+++ RomanNum/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using System;
+using System.Collections.Generic;
+namespace RomanNum
+{
+    public class RomanNumeralConverter
+    {
+        public string Convert(int number)
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
---
+++ RomanNumTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using RomanNum;
+
+namespace RomanNumTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+    
+            public void Convert_Given1_ReturnsI()
+        {
+            var converter = new RomanNumeralConverter();
+            Assert.Equal("I", converter.Convert(1)); 
+        }
+
+    }
+    
+}
